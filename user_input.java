7f8a91abd2313130a5e25ad9a81b624723f91fc4
Developer_kartik_sarkale <sarkalekartik42@gmail.com>
2025-06-29 06:14:21 -0700

    Create user_input.java

---
+++ user_input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class user_input {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter A number : ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum of Tow Num " + sum);
+    }
+ }
