d7ca16f419bfbcc2923ee6b8578a6ed76ed8c6ec
Developer_kartik_sarkale <sarkalekartik42@gmail.com>
2025-06-29 09:28:15 -0700

    Create button.java

---
+++ button.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class button {
+    public static void main(String[] args) {import java.util.*;
+
+public class sum01 {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int button = sc.nextInt();
+        switch (button) {
+            case 1:
+                System.out.println("Hello");
+
+                break;
+            case 2:
+                System.out.println("namste");
+                break;
+            default:
+                System.out.println("imvalid Button");
+                break;
+        }
+    }
+}
