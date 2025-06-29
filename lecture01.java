e8c89a78444085b7b5c5e622c4ddbae56536e79c
Developer_kartik_sarkale <sarkalekartik42@gmail.com>
2025-06-29 06:11:09 -0700

    Create lecture01.java

---
+++ lecture01.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class sum01 {
+    public static void main(String[] args) {
+         int a = 10;
+         int b = 10;
+          int sum = a + b;
+        System.out.println(sum);
+
+      
+       }
+}
