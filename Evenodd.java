9858e907a53e04ffcae01dfd9e0558bb70860e79
Developer_kartik_sarkale <sarkalekartik42@gmail.com>
2025-06-29 09:17:07 -0700

    Create Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class sum01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("odd");
+        }
+}
