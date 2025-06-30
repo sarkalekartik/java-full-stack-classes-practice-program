a81d02593b7aece278fdcb3650d99c01c10e38ca
Developer_kartik_sarkale <sarkalekartik42@gmail.com>
2025-06-29 20:48:34 -0700

    Create Lecture_4.java

---
+++ Lecture_4.java
@@ -0,0 +1,25 @@
+public class Lecture_4 {
+    public static void main(String[] args) {
+
+
+      //print the 100 times hello world
+      
+        // for (int counter = 0; counter < 100; counter = counter + 1) {
+        //     System.out.println("Hello World");
+        // }
+
+
+      // print the number in 0 to 100 useing java in for loop
+      
+      // for (int counter = 0; counter < 101; counter++) {
+      //       System.err.println(counter);
+      //   }
+
+
+
+      
+
+
+      
+    }
+}
